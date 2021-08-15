651ba835ed39d495805f5a129a9323d159f88cde
fazla_rabbi <md.fazlarabbi07@gmail.com>
2021-08-15 10:41:08 +0600

    checking

--- Day02/js/test.js
+++ Day02/js/test.js
@@ -1,6 +1,10 @@
 var x=25;
-var y=25.25;
-if(x==y)
+var y=25;
+/*if(x===y)
+document.write("Value Equal");
+else
+document.write("Value not Equal");*/
+if(x!==y)
 document.write("Value Equal");
 else
 document.write("Value not Equal");
