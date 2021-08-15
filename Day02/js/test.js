56acee68fd8ad152b4fcc2ca18bde187d1bbe12a
fazla_rabbi <md.fazlarabbi07@gmail.com>
2021-08-15 19:56:20 +0600

    switch statement are added into example

--- Day02/js/test.js
+++ Day02/js/test.js
@@ -1,10 +1,26 @@
-var x=25;
+/*var x=25;
 var y=25;
-/*if(x===y)
-document.write("Value Equal");
-else
-document.write("Value not Equal");*/
-if(x!==y)
+if(x===y)
 document.write("Value Equal");
 else
 document.write("Value not Equal");
+if(x!==y)
+document.write("Value Equal");
+else
+document.write("Value not Equal");*/
+
+// switch statement
+//var day=2;
+var day;
+/*switch (day) {
+    case 1:
+        document.write("Sunday");
+    case 2:
+            document.write("Monday");
+        
+        break;
+
+    default:
+        document.write("No date is selected");
+        break;
+}*/
